dfae345dafca9f02207930133a79ce9584e7ae61
Richard Slater <github@richard-slater.co.uk>
2016-10-26 19:22:53 +0100

    Add Validator

---
+++ Application/Validators/SourceModelValidator.cs
@@ -0,0 +1,9 @@
+﻿using Application.Models;
+
+namespace Application.Validators {
+    public class SourceModelValidator {
+        public bool Validate(SourceModel model) {
+            return model != null;
+        }
+    }
+}
---
+++ Tests/ObjectValidatorTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Application.Models;
+using Application.Validators;
+using Should;
+using Xunit;
+
+namespace Tests {
+    public class SourceModelValidatorTests {
+        [Fact]
+        [SuppressMessage("ReSharper", "ExpressionIsAlwaysNull")]
+        public void ShouldFailValidationOnNullModel() {
+            var validator = new SourceModelValidator();
+            SourceModel model = null;
+            var result = validator.Validate(model);
+            result.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void ShouldPassValidationOnNullModel() {
+            var validator = new SourceModelValidator();
+            var model = new SourceModel {Items = new[] {"One", "Two"}};
+            var result = validator.Validate(model);
+            result.ShouldBeTrue();
+        }
+    }
+}
